71f72131a1122f3323c4b77cb5ae3abb58574cc1
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-09-03 14:36:38 +0300

    - ChangeProductPriceCommandHandler oluşturuldu.

---
+++ EventSourcing.Api/Handlers/ProductHandlers/ChangeProductPriceCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using EventSourcing.Api.Commands;
+using EventSourcing.Api.EventStores;
+using MediatR;
+
+namespace EventSourcing.Api.Handlers.ProductHandlers
+{
+    public class ChangeProductPriceCommandHandler : IRequestHandler<ChangeProductPriceCommand>
+    {
+        private readonly ProductStream _productStream;
+
+        public ChangeProductPriceCommandHandler(ProductStream productStream)
+        {
+            _productStream = productStream;
+        }
+
+        public async Task<Unit> Handle(ChangeProductPriceCommand request, CancellationToken cancellationToken)
+        {
+            _productStream.PriceChanged(request.ChangeProductPriceDto);
+
+            await _productStream.SaveAsync();
+
+            return Unit.Value;
+        }
+    }
+}
