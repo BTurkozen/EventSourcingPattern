7ac8038bb1d50eec5d321160d3d5342eb9bdd948
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-09-03 14:34:50 +0300

    - ChangeProductNameCommandHandler oluşturuldu.

---
+++ EventSourcing.Api/Handlers/ProductHandlers/ChangeProductNameCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using EventSourcing.Api.Commands;
+using EventSourcing.Api.EventStores;
+using MediatR;
+
+namespace EventSourcing.Api.Handlers.ProductHandlers
+{
+    public class ChangeProductNameCommandHandler : IRequestHandler<ChangeProductNameCommand>
+    {
+        private readonly ProductStream _productStream;
+
+        public ChangeProductNameCommandHandler(ProductStream productStream)
+        {
+            _productStream = productStream;
+        }
+
+        public async Task<Unit> Handle(ChangeProductNameCommand request, CancellationToken cancellationToken)
+        {
+            _productStream.NameChanged(request.ChangeProductNameDto);
+
+            await _productStream.SaveAsync();
+
+            return Unit.Value;
+        }
+    }
+}
